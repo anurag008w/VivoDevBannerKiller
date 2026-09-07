c7595b64feec9a725c719f32dcfc687fd973c40e
Anurag <anurag@local>
2026-09-07 17:51:04 +0530

    fix: Import android.widget.FrameLayout in MainActivity (v1.5.3)

--- app/src/main/java/com/anurag/devbannerkiller/MainActivity.java
+++ app/src/main/java/com/anurag/devbannerkiller/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
